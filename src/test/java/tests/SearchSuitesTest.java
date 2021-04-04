04deb8e39e0acce384f2448a539d7e6995a5999a
Mohamed Ali <mohamed.ahmed@swvl.com>
2021-04-04 19:22:00 +0200

    add Jenkins file

--- src/test/java/tests/SearchSuitesTest.java
+++ src/test/java/tests/SearchSuitesTest.java
@@ -11,7 +11,7 @@ import properties.TestData;
 
 import java.io.IOException;
 
-public class SearchSuites extends TestBase {
+public class SearchSuitesTest extends TestBase {
     SearchPage searchPage;
     SearchResultPage  searchResultPage;
     MapSearchPage mapSearchPage;
